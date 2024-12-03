406cf1353a37f892dc33fb8ce92db94dc9f25583
Saravanan Prasadh <saravanan.prasadh@starburstdata.com>
2024-12-03 17:40:27 +0530

    Added insecure toggle

--- src/sepdpc/__main__.py
+++ src/sepdpc/__main__.py
@@ -15,7 +15,8 @@ help_text = {
     'user': 'The username you are authenticating with',
     'token': 'The token used for authentication, the part after "Basic" and "-" if no authentication (no password)',
     'domain': 'The domain to filter data products, "none" for no domain filter',
-    'catalog': 'The catalog to filter data products, "none" for no catalog filter'
+    'catalog': 'The catalog to filter data products, "none" for no catalog filter',
+    'insecure': 'Allow insecure connections'
 }
 
 config_path = Path.home() / '.sepdpc'
@@ -38,6 +39,7 @@ UserOpt = _opt(help_text['user'], 'SEPDPC_USER')
 TokenOpt = _opt(help_text['token'], 'SEPDPC_TOKEN')
 DomainOpt = _opt(help_text['domain'], 'SEPDPC_DOMAIN')
 CatalogOpt = _opt(help_text['catalog'], 'SEPDPC_CATALOG')
+insecureOpt = _opt(help_text['insecure'], 'SEPDPC_INSECURE')
 
 app = typer.Typer()
 
@@ -50,12 +52,15 @@ def configure(host: _opt(help_text['host']), user: _opt(help_text['user']), toke
     set_key(dotenv_path=config_path, key_to_set="SEPDPC_TOKEN", value_to_set=token)
     set_key(dotenv_path=config_path, key_to_set="SEPDPC_DOMAIN", value_to_set=domain)
     set_key(dotenv_path=config_path, key_to_set="SEPDPC_CATALOG", value_to_set=catalog)
+    set_key(dotenv_path=config_path, key_to_set="SEPDPC_INSECURE", value_to_set=insecure)
     print('Configured sepdpc')
 
 
 @app.command()
-def generate(host: HostOpt, user: UserOpt, token: TokenOpt, domain: DomainOpt, catalog: CatalogOpt, path: str):
+def generate(host: HostOpt, user: UserOpt, token: TokenOpt, domain: DomainOpt, catalog: CatalogOpt, path: str, insecure: insecureOpt = True):
     if token != '-': token='Basic '+token
+    if insecure:
+        client = SepClient(host=host, user=user, token=token, verify=False)
     client = SepClient(host=host, user=user, token=token)
     repo = repository.from_server(client, domain, catalog)
     repository.persist(repo, path)
@@ -70,8 +75,10 @@ def validate(path: str):
 
 
 @app.command()
-def diff(host: HostOpt, user: UserOpt, token: TokenOpt, domain: DomainOpt, catalog: CatalogOpt, path: str):
+def diff(host: HostOpt, user: UserOpt, token: TokenOpt, domain: DomainOpt, catalog: CatalogOpt, path: str, insecure: insecureOpt = True):
     if token != '-': token='Basic '+token
+    if insecure:
+        client = SepClient(host=host, user=user, token=token, verify=False)
     client = SepClient(host=host, user=user, token=token)
     remote_repo = repository.from_server(client, domain, catalog)
     local_repo = repository.from_local(path)
@@ -100,8 +107,10 @@ def diff(host: HostOpt, user: UserOpt, token: TokenOpt, domain: DomainOpt, catal
 
 
 @app.command()
-def publish(host: HostOpt, user: UserOpt, token: TokenOpt, domain: DomainOpt, catalog: CatalogOpt, path: str):
+def publish(host: HostOpt, user: UserOpt, token: TokenOpt, domain: DomainOpt, catalog: CatalogOpt, path: str, insecure: insecureOpt = True):
     if token != '-': token='Basic '+token
+    if insecure:
+        client = SepClient(host=host, user=user, token=token, verify=False)
     client = SepClient(host=host, user=user, token=token)
     local_repo = repository.from_local(path)
     repository.publish(client, domain, catalog, local_repo)
